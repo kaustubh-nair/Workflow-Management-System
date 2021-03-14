bcdf83829273121485b953a3fff7a312dbe84138
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-03-14 14:53:10 +0530

    Add process table and instance migrations

--- workflow_app/models.py
+++ workflow_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ProcessTemplate(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+
+class Process(models.Model):
+    name = models.CharField(max_length=200)
+    template = models.ForeignKey(ProcessTemplate, on_delete=models.PROTECT)
+
