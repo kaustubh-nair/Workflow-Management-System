0e3a982e92f3b744765410f0d5cc6970a7379472
Kaustubh Nair <kaustubh.nair@iiitb.org>
2021-03-14 15:34:55 +0530

    Add skeletal code (#1)
    * Add index view
    
    * Add process table and instance migrations
    
    * Add all models to admin
    
    * Add task models
    
    * Add actor model
    
    * Add many to many for task-processes

--- workflow_app/models.py
+++ workflow_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class ProcessTemplate(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    tasks = models.ManyToManyField('TaskTemplate')
+
+class Process(models.Model):
+    name = models.CharField(max_length=200)
+    template = models.ForeignKey(ProcessTemplate, on_delete=models.PROTECT)
+
+class TaskTemplate(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    all_or_any = models.BooleanField()
+    choice = models.CharField(max_length=200)
+
+class Actor(models.Model):
+    name = models.CharField(max_length=200)
+
+class Task(models.Model):
+    output = models.CharField(max_length=200)
+    deadline = models.DateTimeField(max_length=200)
+    status = models.CharField(max_length=200)
+    template = models.ForeignKey(TaskTemplate, on_delete=models.PROTECT)
+    process = models.ForeignKey(Process, on_delete=models.CASCADE)
+    actors = models.ManyToManyField(Actor)
+
+class TaskSequence(models.Model):
+    parent = models.ForeignKey(TaskTemplate, on_delete=models.CASCADE, related_name='parent')
+    child = models.ForeignKey(TaskTemplate, on_delete=models.CASCADE, related_name='child')
