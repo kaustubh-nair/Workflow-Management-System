219b9f971395b6d05cdf66f0857aa913d602ba5c
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-03-29 11:57:11 +0530

    Add prototype code for the whole application

--- workflow_app/views.py
+++ workflow_app/views.py
@@ -4,14 +4,19 @@ from django.http import HttpResponse
 from . import models
 
 def index(request):
-    # 
-    ta = models.TaskTemplate.objects.create(name="taska", all_or_any=False)
-    tb = models.TaskTemplate.objects.create(name="taskb", all_or_any=False)
-    tc = models.TaskTemplate.objects.create(name="taskc", all_or_any=False)
+    pa = models.ProcessTemplate.objects.create(name="Paper review")
+    process = models.Process.objects.create(name="Paper review IIITB", template=pa)
+
+    ta = models.TaskTemplate.objects.create(name="Initialize", all_or_any=False)
+    tb = models.TaskTemplate.objects.create(name="Review", all_or_any=False)
+    tc = models.TaskTemplate.objects.create(name="Accept/Reject", all_or_any=False)
+    seqa = models.TaskSequence.objects.create(parent=ta, child=tb)
+    seqb = models.TaskSequence.objects.create(parent=tb, child=tc)
+
+
     ra = models.Role.objects.create(name="rolea")
     rb = models.Role.objects.create(name="roleb")
     rc = models.Role.objects.create(name="rolec")
-    pa = models.ProcessTemplate.objects.create(name="processa")
     actor = models.Actor.objects.create(name="boo")
     actor.roles.add(ra)
     actor.roles.add(rc)
@@ -20,16 +25,33 @@ def index(request):
     actor2.roles.add(ra)
     actor2.roles.add(rb)
     actor2.save()
-    process = models.Process.objects.create(name="fuc", template=pa)
 
 
     tasks = []
     t = [ta, tb, tc]
-    a = [actor, actor, actor2]
     for i in range(3):
-        task = models.Task.objects.create(output="ASD" + str(i),deadline=datetime.now(), template=t[i], process=process)
-        task.actors.add(a[i])
-        task.save()
+        task = models.Task.objects.create(output="output" + str(i),deadline=datetime.now(), template=t[i], process=process, status="Incomplete")
         tasks.append(task)
 
-    return HttpResponse(tasks[1].template.name)
+    tasks[0].status = "Ongoing"  # Add constraints for permissible values.
+    tasks[0].save()
+
+    tasks[1].status = "Ongoing"
+    tasks[1].save()
+
+    tasks[2].status = "Ongoing"
+    tasks[2].save()
+
+    tasks[0].status = "Complete"
+    tasks[0].actors.add(actor)  # Add constraint for role
+    tasks[0].save()
+
+    tasks[1].status = "Complete"
+    tasks[1].actors.add(actor)
+    tasks[1].save()
+
+    tasks[2].status = "Complete"
+    tasks[2].actors.add(actor2)
+    tasks[2].save()
+
+    return HttpResponse(tasks[1].status)
