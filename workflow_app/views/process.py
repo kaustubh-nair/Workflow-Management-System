5357f8494ffb0651d9b883bef58f0c8a63d90d47
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-04-05 00:23:03 +0530

    Add basic html for process creation

--- workflow_app/views/process.py
+++ workflow_app/views/process.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from datetime import datetime
+from django.template import loader
 from django.http import HttpResponse
 
 from ..models import ProcessTemplate
-from .forms import SomeForm
 
-def index(request):
-    return render(request, 'create_process.html', {'form': SomeForm()})
+def create_template(request):
+    context = {'process_template': process_template}
+    return render(request, 'create_process.html', context)
