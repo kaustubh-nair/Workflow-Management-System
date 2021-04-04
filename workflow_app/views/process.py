fe5d025bb99b62c76356e6d1b02f7a28b6f88e56
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-04-04 15:57:31 +0530

    Split views to different files

--- workflow_app/views/process.py
+++ workflow_app/views/process.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from datetime import datetime
 from django.http import HttpResponse
 
-from .models import Role
+from ..models import Role
 
 def index(request):
     role = Role()
