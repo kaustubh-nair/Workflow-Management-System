990fe42bc95402c19a5b150e547af2c0cb86ef47
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-04-05 12:04:34 +0530

    Add multiple children to task sequence

--- workflow_app/models/process_template.py
+++ workflow_app/models/process_template.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class ProcessTemplate(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
-    tasks = models.ManyToManyField('TaskTemplate')
+    first_task = models.ForeignKey('Task', on_delete=models.PROTECT, default=1)
 
--- workflow_app/models/task_sequence.py
+++ workflow_app/models/task_sequence.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 from .task_template import TaskTemplate
+from .process_template import ProcessTemplate
 
 class TaskSequence(models.Model):
     parent = models.ForeignKey(TaskTemplate, on_delete=models.CASCADE, related_name='parent')
-    child = models.ForeignKey(TaskTemplate, on_delete=models.CASCADE, related_name='child')
+    children = models.ManyToManyField(TaskTemplate)
+    process_template = models.ForeignKey(ProcessTemplate, on_delete=models.CASCADE, default=1)
