cfa32dd7f90b9601749eec1a30e2bff0f9e9341b
Ram-is-me <ram.s@iiitb.org>
2021-04-06 18:26:36 +0530

    URL and view template setup

--- workflow_app/urls.py
+++ workflow_app/urls.py
@@ -4,10 +4,14 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
+<<<<<<< HEAD
         path('process/template/create/', views.process_template.create_template, name='create_process_template'),
         path('', views.sessions.home, name='home'),
         path('viewdefs', views.sessions.viewdefs, name = 'viewdefs'),
         path('accounts/signup', views.sessions.SignUpView.as_view(), name='signup'),
+=======
+        path('process/template/create/', views.process.create_template, name='create_process_template'),
+>>>>>>> URL and view template setup
         path('', views.process.index, name='index'),
         path('execindex', views.execution.index, name='executionindex'),
 ]
