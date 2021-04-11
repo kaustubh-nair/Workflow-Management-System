3f77068b822b147ec7110a343f8b6e2efad1a13c
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-04-12 00:12:24 +0530

    Restrict based on field

--- workflow_app/views/process.py
+++ workflow_app/views/process.py
@@ -7,12 +7,40 @@ from ..models import ProcessTemplate
 from .forms import ProcessTemplateForm
 
 def create_template(request):
+    tasks = {}
     if request.method == 'POST':
         form = ProcessTemplateForm(request.POST)
         if form.is_valid():
             form.save()
     else:
+        for k,v in dict(request.GET).items():
+            if (k.startswith('task')):
+                task_number = int(k[4:6])
+
+                if task_number not in tasks:
+                    tasks[task_number] = {}
+
+                keyword = k[6:]
+                if keyword == 'name':
+                    task['name'] = v
+                    tasks[task_number]['name'] = v
+                elif keyword == 'description':
+                    task['description'] = v
+                    tasks[task_number]['description'] = v
+                elif keyword == 'all_or_any':
+                    task['all_or_any'] = v
+                    tasks[task_number]['all_or_any'] = v
+                elif keyword == 'choice':
+                    task['choice'] = v
+                    tasks[task_number]['choice'] = v
+                elif keyword == 'role':
+                    task['role'] = v
+                    tasks[task_number]['role'] = v
+
+
         form = ProcessTemplateForm()
 
-    context = {'form': form}
+    print(tasks)
+    context = {'form': form, 'tasks': tasks}
+
     return render(request, 'create_process_template.html', context)
