02bf72a3f14603d5b79f9b343aa39075a36ff2e7
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-04-11 12:56:26 +0530

    Add form for creating process template

--- workflow_app/views/forms.py
+++ workflow_app/views/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
-class SomeForm(forms.Form):
-    your_name = forms.CharField(max_length=100)
+from ..models import ProcessTemplate
+
+class ProcessTemplateForm(forms.Form):
+    name = forms.CharField(max_length=200)
+    description = forms.CharField(max_length=1000)
--- workflow_app/views/process.py
+++ workflow_app/views/process.py
@@ -4,7 +4,15 @@ from django.template import loader
 from django.http import HttpResponse
 
 from ..models import ProcessTemplate
+from .forms import ProcessTemplateForm
 
 def create_template(request):
-    context = {'process_template': process_template}
-    return render(request, 'create_process.html', context)
+    if request.method == 'POST':
+        form = ProcessTemplateForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProcessTemplateForm()
+
+    context = {'form': form}
+    return render(request, 'create_process_template.html', context)
