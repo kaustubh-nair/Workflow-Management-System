52d7f0003ac2f51c3fe4b6f18a0866f1c38c7756
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-04-11 23:08:00 +0530

    Add task fields to form

--- workflow_app/views/forms.py
+++ workflow_app/views/forms.py
@@ -5,3 +5,9 @@ from ..models import ProcessTemplate
 class ProcessTemplateForm(forms.Form):
     name = forms.CharField(max_length=200)
     description = forms.CharField(max_length=1000)
+
+    task_name = forms.CharField(max_length=200)
+    task_description = forms.CharField(max_length=1000)
+    task_all_or_any = forms.BooleanField()
+    task_choice = forms.CharField(max_length=200)
+    task_role = forms.CharField(max_length=200)
