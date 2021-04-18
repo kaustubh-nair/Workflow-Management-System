e6b5b954a41cd07e7cbc1bc20868bfe021e1063f
R4hu1M5 <rahulms.nov@gmail.com>
2021-04-18 16:22:58 +0530

    Get login to work

--- workflow_app/urls.py
+++ workflow_app/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
         path('process/template/create/', views.process_template.create_template, name='create_process_template'),
+        path('', views.sessions.home, name='home'),
+        path('viewdefs', views.sessions.viewdefs, name = 'viewdefs')
 ]
---
+++ workflow_app/views/sessions.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import TemplateView
+from django.views import generic
+from django.urls import reverse_lazy, reverse
+from ..models import ProcessTemplate
+
+# class SignUpView(generic.CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'signup.html'
+
+def home(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('viewdefs'))
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
+def viewdefs(request):
+    pt_list = ProcessTemplate.objects.all()
+    context = {'pt_list' : pt_list}
+    return render(request, 'viewdefinitions.html', context)
