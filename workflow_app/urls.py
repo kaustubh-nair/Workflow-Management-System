b28d0054a9cc7d903fb163a2a248a0f3495f9db1
Ram-is-me <ram.s@iiitb.org>
2021-04-19 10:33:06 +0530

    Partial All or Any Logic Implemented

--- workflow_app/urls.py
+++ workflow_app/urls.py
@@ -4,11 +4,11 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-        path('process/template/create/', views.process_template.create_template, name='create_process_template'),
+        # path('process/template/create/', views.process_template.create_template, name='create_process_template'),
         path('', views.sessions.home, name='home'),
         path('viewdefs', views.sessions.viewdefs, name = 'viewdefs'),
         path('accounts/signup', views.sessions.SignUpView.as_view(), name='signup'),
-        path('process/template/create/', views.process.create_template, name='create_process_template'),
+        # path('process/template/create/', views.process.create_template, name='create_process_template'),
         # path('', views.process.index, name='index'),
         # path('login/', views.login.index, name='login'),
         path('viewexec/<int:exec_id>/', views.execution.index, name='executionindex'),
--- workflow_management_system/settings.py
+++ workflow_management_system/settings.py
@@ -128,26 +128,26 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGGING = {
-    'version': 1,
-    'filters': {
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'level': 'DEBUG',
-            'handlers': ['console'],
-        }
-    }
-}
+# LOGGING = {
+#     'version': 1,
+#     'filters': {
+#         'require_debug_true': {
+#             '()': 'django.utils.log.RequireDebugTrue',
+#         }
+#     },
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'filters': ['require_debug_true'],
+#             'class': 'logging.StreamHandler',
+#         }
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'level': 'DEBUG',
+#             'handlers': ['console'],
+#         }
+#     }
+# }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
