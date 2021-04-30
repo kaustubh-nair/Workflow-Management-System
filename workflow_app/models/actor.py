f1a0e03dadcf6444d83290fb84aa2417d14ddf21
R4hu1M5 <rahulms.nov@gmail.com>
2021-04-30 17:19:05 +0530

    Add group to Actor model, change seed data

--- workflow_app/models/actor.py
+++ workflow_app/models/actor.py
@@ -5,4 +5,6 @@ from .role import Role
 class Actor(models.Model):
     name = models.CharField(max_length=200)
     roles = models.ManyToManyField(Role)
+    group_choices = ('CU', 'CreationUser'), ('EU', 'ExecutionUser')
+    group = models.CharField(max_length=2, choices=group_choices)
 
