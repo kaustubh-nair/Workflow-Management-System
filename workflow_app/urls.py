ec96383a6e8407dcee2dba371153638ce644f145
Ram-is-me <ram.s@iiitb.org>
2021-04-30 19:30:05 +0530

    Delete Execution

--- workflow_app/urls.py
+++ workflow_app/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
         path('process/template/<int:process_id>/edit/', views.process.edit, name='edit_process_template'),
         path('task/template/<int:task_template_id>/edit/', views.process.edit_task, name='edit_process'),
         path('create/exec/<int:template_id>', views.execution.create_exec, name='create_execution'),
+        path('delete/exec/<int:execution_id>', views.execution.delete_exec, name='delete_execution'),
         path('viewexec/<int:exec_id>/', views.execution.index, name='executionindex'),
         path('viewexec/<int:exec_id>/completetask/<int:task_id>/<str:action>', views.execution.completeTask, name='completing task'),
         path('accounts/signup', views.sessions.signup, name='signup'),
--- workflow_app/views/execution.py
+++ workflow_app/views/execution.py
@@ -20,91 +20,18 @@ from ..models import Role
 from ..models import Task
 from ..models import Process
 
-
-def init():
-    role1 = Role(name = "Role 1")
-    role1.save()
-    role2 = Role(name = "Role 2")
-    role2.save()
+@login_required
+def delete_exec(request, execution_id):
+    # return HttpResponse("LOL")
+    current_exec = Process.objects.filter(id=execution_id).get()
+    current_template = current_exec.template
+    list_of_tasks = Task.objects.filter(process = current_exec)
+    for task in list_of_tasks:
+        task.delete()
     
-    actor1 = Actor(name = "Actor 1")
-    actor1.save()
-    actor1.roles.add(role1)
-    actor1.save()
-    actor2 = Actor(name = "Actor 2")
-    actor2.save()
-    actor2.roles.add(role1)
-    actor2.roles.add(role2)
-    actor2.save()
-    actor3 = Actor(name = "Actor 3")
-    actor3.save()
-    actor3.roles.add(role2)
-    actor3.save()
-    
-    pt1 = ProcessTemplate(name = "Process Template 1", description = "Description of Process Template 1", creator = actor1, dateOfCreation = datetime.now())
-    pt1.save()
-    pt2 = ProcessTemplate(name = "Process Template 2", description = "Description of Process Template 2", creator = actor1, dateOfCreation = datetime.now())
-    pt2.save()
+    current_exec.delete()
 
-    tt11 = TaskTemplate(name = "Task Template 11", description = "Description of Task Template 11", all_or_any = False, choice = "Choice 11", role=role1, process_template = pt1, is_first_task = True)
-    tt11.save()
-    tt12 = TaskTemplate(name = "Task Template 12", description = "Description of Task Template 12", all_or_any = True, choice = "Choice 12", role=role2, process_template = pt1, is_first_task = False)
-    tt12.save()
-    tt11.children.add(tt12)
-    tt11.save()
-    tt13 = TaskTemplate(name = "Task Template 13", description = "Description of Task Template 13", all_or_any = False, choice = "Choice 13", role=role1, process_template = pt1, is_first_task = False)
-    tt13.save()
-    tt12.children.add(tt13)
-    tt12.save()
-
-    tt21 = TaskTemplate(name = "Task Template 21", description = "Description of Task Template 21", all_or_any = False, choice = "Choice 21", role=role1, process_template = pt2, is_first_task = True)
-    tt21.save()
-    tt22 = TaskTemplate(name = "Task Template 22", description = "Description of Task Template 22", all_or_any = True, choice = "Choice 22", role=role2, process_template = pt2, is_first_task = False)
-    tt22.save()
-    tt21.children.add(tt22)
-    tt21.save()
-    tt23 = TaskTemplate(name = "Task Template 23", description = "Description of Task Template 23", all_or_any = False, choice = "Choice 23", role=role1, process_template = pt2, is_first_task = False)
-    tt23.save()
-    tt22.children.add(tt23)
-    tt22.save()
-    tt24 = TaskTemplate(name = "Task Template 24", description = "Description of Task Template 24", all_or_any = False, choice = "Choice 24", role=role2, process_template = pt1, is_first_task = False)
-    tt24.save()
-    tt23.children.add(tt24)
-    tt23.save()
-
-    # First Number template, second number iteration
-    e11 = Process(name = "Execution 11", template = pt1, creator = actor2, dateOfCreation = datetime.now())
-    e11.save()
-    deadlinee1 = datetime.now()
-    deadlinee1 = deadlinee1.replace(hour=12)
-    deadlinee2 = datetime.now()
-    deadlinee2 = deadlinee2.replace(second=30)
-    et111 = Task(output = "Output of Task 1 Execution 1", deadline = deadlinee1, status = "Started", template = tt11, process = e11)
-    et111.save()
-    et112 = Task(output = "Output of Task 2 Execution 1", deadline = deadlinee2, status = "Not Started", template = tt12, process = e11)
-    et112.save()
-    et113 = Task(output = "Output of Task 3 Execution 1", deadline = deadlinee1, status = "Not Started", template = tt13, process = e11)
-    et113.save()
-
-    e12 = Process(name = "Execution 12", template = pt1, creator = actor2, dateOfCreation = datetime.now())
-    e12.save()
-    et121 = Task(output = "Output of Task 1 Execution 2", deadline = deadlinee1, status = "Started", template = tt11, process = e12)
-    et121.save()
-    et122 = Task(output = "Output of Task 2 Execution 2", deadline = deadlinee2, status = "Not Started", template = tt12, process = e12)
-    et122.save()
-    et123 = Task(output = "Output of Task 3 Execution 2", deadline = deadlinee1, status = "Not Started", template = tt13, process = e12)
-    et123.save()
-
-    e21 = Process(name = "Execution 21", template = pt2, creator = actor1, dateOfCreation = datetime.now())   
-    e21.save()
-    et211 = Task(output = "Output of Task 1 Execution 1", deadline = deadlinee1, status = "Started", template = tt21, process = e21)
-    et211.save()
-    et212 = Task(output = "Output of Task 2 Execution 1", deadline = deadlinee2, status = "Not Started", template = tt22, process = e21)
-    et212.save()
-    et213 = Task(output = "Output of Task 3 Execution 1", deadline = deadlinee1, status = "Not Started", template = tt23, process = e21)
-    et213.save()
-    et214 = Task(output = "Output of Task 4 Execution 1", deadline = deadlinee1, status = "Not Started", template = tt24, process = e21)
-    et214.save()
+    return HttpResponseRedirect(reverse('viewexecs',args=(current_template.id,)))
 
 @login_required
 def create_exec(request, template_id):
@@ -128,7 +55,7 @@ def create_exec(request, template_id):
 
 @login_required
 def index(request, exec_id):
-    exec_name = "Name of Execution"
+    exec_obj = Process.objects.filter(id = exec_id).get()
     task_list = Task.objects.filter(process_id=exec_id)
     total_action_list = []
     for task in task_list:
@@ -148,7 +75,7 @@ def index(request, exec_id):
     collapse_show = "collapse show"
     collapse = "collapse"
     context = {
-        'exec_name' : exec_name,
+        'exec_obj': exec_obj,
         'task_list' : task_list,
         'user_roles' : user_roles,
         'total_action_list': total_action_list,
